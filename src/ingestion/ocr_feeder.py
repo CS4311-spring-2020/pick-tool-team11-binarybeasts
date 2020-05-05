58a1039bf2c2cd4bbdd10ba127398cfdc0546bc9
pm511 <pmendoza7@miners.utep.edu>
2020-05-05 14:06:01 -0600

    Added arguments to OCR so that we can run it by typing the filename instead of hardcoding it

--- src/ingestion/ocr_feeder.py
+++ src/ingestion/ocr_feeder.py
@@ -4,17 +4,20 @@
 import pytesseract
 from PIL import Image
 import pandas as pd
+import sys
 
 # add path to the tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\jlluj\AppData\Local\Tesseract-OCR\tesseract.exe'
+if __name__== "__main__":
 
-# add path to the image
-img = Image.open(r'C:\Users\jlluj\Documents\PICK\pick-tool-team11-binarybeasts\src\ingestion\testimage.jpg')
-text = pytesseract.image_to_string(img)
-print(text)
-saveFile = open('ingestion\ImageToText.txt', 'w')
-saveFile.write(text)
-saveFile.close()
+    # Run the file by typing ocr_feeder.py "filename.png" inside the directory
+    img=Image.open(sys.argv[1])
 
-data = pd.read_csv("ImageToText.txt",delimiter=',')
-data.to_csv(r'C:\Users\jlluj\Documents\PICK\pick-tool-team11-binarybeasts\src\ingestion\imageCSV.csv')
+    text = pytesseract.image_to_string(img)
+    print(text)
+    saveFile = open('ingestion\ImageToText.txt', 'w')
+    saveFile.write(text)
+    saveFile.close()
+
+    data = pd.read_csv("ImageToText.txt",delimiter=',')
+    data.to_csv(r'C:\Users\jlluj\Documents\PICK\pick-tool-team11-binarybeasts\src\ingestion\imageCSV.csv')
