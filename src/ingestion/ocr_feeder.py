dabe256b22791d2e8d281542e7cfab441c3781b9
pm511 <pmendoza7@miners.utep.edu>
2020-05-05 10:58:56 -0600

    image to text file can now turn text file to csv file

--- src/ingestion/ocr_feeder.py
+++ src/ingestion/ocr_feeder.py
@@ -3,6 +3,8 @@
 
 import pytesseract
 from PIL import Image
+import pandas as pd
+
 # add path to the tesseract executable
 pytesseract.pytesseract.tesseract_cmd = r'C:\Users\jlluj\AppData\Local\Tesseract-OCR\tesseract.exe'
 
@@ -13,3 +15,6 @@ print(text)
 saveFile = open('ingestion\ImageToText.txt', 'w')
 saveFile.write(text)
 saveFile.close()
+
+data = pd.read_csv("ImageToText.txt",delimiter=',')
+data.to_csv(r'C:\Users\jlluj\Documents\PICK\pick-tool-team11-binarybeasts\src\ingestion\imageCSV.csv')
