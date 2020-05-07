da6d0a4356de23db8c220e81dc197c2b72f57b92
pm511 <pmendoza7@miners.utep.edu>
2020-05-06 22:53:56 -0600

    attempted to connect cleansing to PICK tool

--- src/ingestion/cleansing.py
+++ src/ingestion/cleansing.py
@@ -4,6 +4,9 @@ import regex as re
 import pandas as pd
 import numpy as np
 import csv
+from configuration.configurations import Configuration
+from threading import Thread
+from ingestion.splunk_interface import SplunkInterface
 
 class Cleansing:
 
@@ -23,5 +26,17 @@ class Cleansing:
 
         #Store the dataframe as a new CSV
         data.to_csv(r'C:\pathToTheFile\cleansed.csv',index=False)
+
+
+configuration = Configuration.get_instance()
+if configuration.splunk != None:
+all_log_entries = configuration.splunk.get_log_entries()
+
+if (all_log_entries == )
+
+    def get_log_entries_thread(self, configuration):
+        if configuration.splunk:
+            all_log_entries = configuration.splunk.get_log_entries()
+            self.insert_log_entry_search_data(all_log_entries)
 
 
