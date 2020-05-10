5d49727c2683a2ba771d09a3290b7ee86b4f2cbc
pm511 <38440664+pm511@users.noreply.github.com>
2020-05-09 19:59:47 -0600

    Cleaned out cleansing.py by removing the commented code

--- src/ingestion/cleansing.py
+++ src/ingestion/cleansing.py
@@ -5,12 +5,6 @@ import numpy as np
 import csv
 import sys
 
-#from configuration.configurations import Configuration
-#from threading import Thread
-#from ingestion.splunk_interface import SplunkInterface
-
-#class Cleansing:
-
     #def cleanse(str):
 if __name__== "__main__":
     
@@ -30,17 +24,4 @@ if __name__== "__main__":
     # Store the dataframe as a new CSV
     # Give the location you would like to have the file saved.
     data.to_csv(r'C:\locationPath\Cleansed.csv',index=False)
-
-
-#configuration = Configuration.get_instance()
-#if configuration.splunk != None:
-#all_log_entries = configuration.splunk.get_log_entries()
-
-#if (all_log_entries == )
-
-#    def get_log_entries_thread(self, configuration):
-#        if configuration.splunk:
-#            all_log_entries = configuration.splunk.get_log_entries()
-#            self.insert_log_entry_search_data(all_log_entries)
-
 
